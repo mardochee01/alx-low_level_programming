818eca8db765279f78f757ff8b617119dde921ee
mardochee01 <mardoche.gneran@gmail.com>
2022-03-11 16:27:11 -0800

    putchar ascii

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = 48 ; num <= 57 ; num++)
+		putchar(num);
+	for (alpha = 97 ; alpha <= 102 ; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}

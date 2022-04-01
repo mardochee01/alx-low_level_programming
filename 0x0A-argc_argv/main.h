bdf0f1a6fe036cbb877eb3c4ebc4f4ef158ec880
mardochee01 <mardoche.gneran@gmail.com>
2022-04-01 05:55:41 -0700

    Task 0 to 4

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Mardochee01
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0A. C - argc, argv.
+ */
+
+int _putchar(char c);
+int _atoi(char *s);
+#endif
